572cf6d68b4189bd8b76a1048a386e876782ece8
flywind <xzsflywind@gmail.com>
2020-08-10 21:59:16 +0800

    clean

--- src/ioselectors/timerwheel.nim
+++ src/ioselectors/timerwheel.nim
@@ -1,13 +1,14 @@
-import std/monotimes
 import lists
+import math
 
 
 const
   widthBits {.strdefine.} = 8
   numLevels = (64 + widthBits - 1) div widthBits
-  numSlots = 1 shl 8
+  numSlots = 1 shl widthBits
   mask = numSlots - 1
 
+assert isPowerOfTwo(widthBits)
 
 type
   Tick* = Natural
@@ -18,7 +19,7 @@ type
     finishAt*: Tick
     cb*: Callback
 
-  TimerEventList* = DoublyLinkedList[TimerEvent]
+  TimerEventList* =  DoublyLinkedRing[TimerEvent]
 
   Timer* = object
     interval*: Tick
@@ -37,8 +38,9 @@ proc initTimerEvent*(finishAt: Tick, cb: Callback): TimerEvent =
   TimerEvent(finishAt: finishAt, cb: cb)
 
 proc setTimer*(s: var Scheduler, event: TimerEvent, levels = 0) =
+  # mod (2 ^ n - 1)
   let scheduleAt = (s.timer.now[levels] + event.finishAt) and mask
-  s.timer.slots[0][scheduleAt].append event
+  s.timer.slots[levels][scheduleAt].append event
 
 proc processTimer*(s: var Scheduler, step: Tick, levels = 0) =
   var scheduleAt = s.timer.now[levels]
