eba10db5b22f0d73eeeb364128e0a3a6c0d2f4c1
flywind <xzsflywind@gmail.com>
2020-08-10 17:04:14 +0800

    init

--- src/ioselectors/timerwheel.nim
+++ src/ioselectors/timerwheel.nim
@@ -2,21 +2,48 @@ import std/monotimes
 import lists
 
 
+const
+  widthBits {.strdefine.} = 8
+  numLevels = (64 + widthBits - 1) div widthBits
+  numSlots = 1 shl 8
+  mask = numSlots - 1
+
+
 type
-  CallBack* = proc() {.gcsafe.}
+  Tick* = Natural
 
-  TimerCallBack* = object
-    finishAt*: MonoTime
-    callback*: CallBack
+  Callback* = proc() {.gcsafe.}
 
-  TimerCallBackList* = SinglyLinkedList[TimerCallBack]
+  TimerEvent* = object
+    finishAt*: Tick
+    cb*: Callback
+
+  TimerEventList* = DoublyLinkedList[TimerEvent]
 
   Timer* = object
-    startTime*: Natural
-    interval*: Natural
-    first*: array[60, TimerCallBackList]
-    others*: array[5, array[60, TimerCallBackList]]
+    interval*: Tick
+    ticksPending*: Tick
+    now*: array[numLevels, Tick]
+    slots*: array[numLevels, array[numSlots, TimerEventList]]
     head, tail: int
 
   Scheduler* = object
     timer: Timer
+
+proc initScheduler*(): Scheduler =
+  discard
+
+proc initTimerEvent*(finishAt: Tick, cb: Callback): TimerEvent =
+  TimerEvent(finishAt: finishAt, cb: cb)
+
+proc setTimer*(s: var Scheduler, event: TimerEvent, levels = 0) =
+  let scheduleAt = s.timer.now[levels] + event.finishAt
+  s.timer.slots[0][scheduleAt].append event
+
+proc processTimer*(s: var Scheduler, step: Tick, levels = 0) =
+  var scheduleAt = s.timer.now[levels]
+  for i in 0 ..< step:
+    for event in s.timer.slots[levels][scheduleAt]:
+      event.cb()
+
+    inc scheduleAt
