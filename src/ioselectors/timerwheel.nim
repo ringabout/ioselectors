4d5f0bbdb44c48c2cd09a964b473c0b5b7186032
flywind <xzsflywind@gmail.com>
2020-08-12 08:23:47 +0800

    so hard

--- src/ioselectors/timerwheel.nim
+++ src/ioselectors/timerwheel.nim
@@ -18,7 +18,7 @@ type
 
   Callback* = proc() {.gcsafe.}
 
-  TimerEvent* = ref object
+  TimerEvent* = object
     finishAt: Tick
     cb*: Callback
 
@@ -28,7 +28,6 @@ type
     duration*: array[numLevels, Tick]
     currentTime*: Tick
     now*: array[numLevels, Tick]
-    bits*: uint32
     slots*: array[numLevels, array[numSlots, TimerEventList]]
 
   Scheduler* = object
@@ -39,14 +38,17 @@ proc initScheduler*(): Scheduler =
   for level in 0 ..< numLevels:
     result.timer.duration[level] = numSlots ^ (level + 1)
 
+proc isActive*(s: Scheduler): bool =
+  s.taskCounter != 0
+
 proc initTimerEvent*(cb: Callback): TimerEvent =
   TimerEvent(cb: cb)
 
 proc `$`*(t: TimerEvent): string =
   $(t.finishAt, )
 
-proc cancel*(t: var TimerEvent) =
-  t.cb = nil
+# proc cancel*(t: var TimerEvent) =
+#   t.cb = nil
 
 proc execute*(t: TimerEvent) =
   if t.cb != nil:
@@ -75,13 +77,15 @@ proc setTimer*(s: var Scheduler, event: var TimerEvent, timeout: Tick) =
 
 proc degradeTimer*(s: var Scheduler, hlevel: Tick) =
   let idx = s.timer.now[hlevel] - 1
-  for event in s.timer.slots[hlevel][idx].mitems:
-    if event.finishAt <= s.timer.currentTime:
-      event.execute()
-    else:
-      s.setTimer(event, event.finishAt - s.timer.currentTime)
-    dec s.taskCounter
-  s.timer.slots[hlevel][idx].head = nil
+
+  if idx >= 0:
+    for event in s.timer.slots[hlevel][idx].mitems:
+      if event.finishAt <= s.timer.currentTime:
+        event.execute()
+      else:
+        s.setTimer(event, event.finishAt - s.timer.currentTime)
+      dec s.taskCounter
+    s.timer.slots[hlevel][idx].head = nil
 
 proc processTimer*(s: var Scheduler, step: Tick) =
   let level = 0
@@ -89,25 +93,25 @@ proc processTimer*(s: var Scheduler, step: Tick) =
   var scheduleAt = s.timer.now[level]
   inc(s.timer.currentTime, step)
 
-  if s.taskCounter > 0:
-    for i in 0 ..< step:
-      let index = scheduleAt and mask
-      for event in s.timer.slots[level][index]:
-        event.execute()
-        dec s.taskCounter
+  # if s.taskCounter > 0:
+  for i in 0 .. step:
+    let index = scheduleAt and mask
+    for event in s.timer.slots[level][index]:
+      event.execute()
+      dec s.taskCounter
 
-      s.timer.slots[level][index].head = nil
+    s.timer.slots[level][index].head = nil
 
-      scheduleAt = (scheduleAt + 1) and mask
+    scheduleAt = (scheduleAt + 1) and mask
 
-      var hlevel = level + 1
-      if scheduleAt == 0 and hlevel < numLevels - 1:
-        inc s.timer.now[hlevel]
+    var hlevel = level + 1
+    if scheduleAt == 0 and hlevel < numLevels - 1:
+      s.timer.now[hlevel] = (s.timer.now[hlevel] + 1) and mask
+      degradeTimer(s, hlevel)
+      while s.timer.now[hlevel] == 0 and hlevel < numLevels - 1:
+        inc hlevel
+        s.timer.now[hlevel] = (s.timer.now[hlevel] + 1) and mask
         degradeTimer(s, hlevel)
-        while s.timer.now[hlevel] == 0 and hlevel < numLevels - 1:
-          inc hlevel
-          degradeTimer(s, hlevel)
-          inc s.timer.now[hlevel]
 
   s.timer.now[level] = scheduleAt
 
@@ -115,29 +119,30 @@ proc processTimer*(s: var Scheduler, step: Tick) =
 when isMainModule:
   import sugar
 
-  var s = initScheduler()
-  var count = 0
-  var event0 = initTimerEvent(proc() = 
-    inc count)
+  # block:
+  #   var s = initScheduler()
+  #   var count = 0
+  #   var event0 = initTimerEvent(proc() = 
+  #     inc count)
 
 
-  var event1 = initTimerEvent(proc() = echo "first")
-  var event2 = initTimerEvent(proc() = echo "second")
+  #   var event1 = initTimerEvent(proc() = echo "first")
+  #   var event2 = initTimerEvent(proc() = echo "second")
 
 
-  s.setTimer(event0, 5)
-  echo s.timer.slots
-  s.processTimer(4)
-  dump count
-  s.processTimer(2)
-  dump count
+  #   s.setTimer(event0, 5)
+  #   echo s.timer.slots
+  #   s.processTimer(4)
+  #   dump count
+  #   s.processTimer(2)
+  #   dump count
 
-  dump s.timer.slots
-  s.setTimer(event0, 5)
-  dump s.timer.slots
-  event0.cancel()
-  s.processTimer(6)
-  dump count
+  #   dump s.timer.slots
+  #   s.setTimer(event0, 5)
+  #   dump s.timer.slots
+  #   event0.cancel()
+  #   s.processTimer(6)
+  #   dump count
 
 
   # s.setTimer(event1, 3)
@@ -155,3 +160,71 @@ when isMainModule:
   # dump s.taskCounter
   # s.processTimer(7)
   # dump s.taskCounter
+
+  block:
+    var s = initScheduler()
+    var count = 0
+    var event0 = initTimerEvent(proc() = 
+      inc count)
+
+    # Scheduler does nothing
+    s.processTimer(10)
+    doAssert count == 0
+    doAssert not s.isActive
+
+    s.setTimer(event0, 5)
+    doAssert s.isActive
+    s.processTimer(5)
+    doAssert count == 1
+
+    s.processTimer(256)
+    doAssert count == 1
+
+    s.setTimer(event0, 5)
+    s.processTimer(5)
+    doAssert count == 2
+
+    # # Canceled timers don't run.
+    # s.setTimer(event0, 5)
+    # event0.cancel()
+    # s.processTimer(10)
+    # doAssert count == 2
+
+    s.processTimer(250)
+    s.setTimer(event0, 5)
+    s.processTimer(10)
+    doAssert count == 3
+
+    # // Timers that are scheduled multiple times only run at the last
+    # // scheduled tick.
+    dump s.timer.now
+    dump s.timer.duration
+    dump s.timer.currentTime
+    s.setTimer(event0, 5)
+    s.setTimer(event0, 10)
+    dump s.timer.slots
+    s.processTimer(5)
+    doAssert count == 3
+
+    # timers.schedule(&timer, 5);
+    # timers.schedule(&timer, 10);
+    # timers.advance(5);
+    # EXPECT_INTEQ(count, 3);
+    # timers.advance(5);
+    # EXPECT_INTEQ(count, 4);
+
+    # // Timer can safely be canceled multiple times.
+    # timers.schedule(&timer, 5);
+    # timer.cancel();
+    # timer.cancel();
+    # EXPECT(!timer.active());
+    # timers.advance(10);
+    # EXPECT_INTEQ(count, 4);
+
+    # {
+    #     TimerEvent<Callback> timer2([&count] () { ++count; });
+    #     timers.schedule(&timer2, 5);
+    # }
+    # timers.advance(10);
+    # EXPECT_INTEQ(count, 4);
+
