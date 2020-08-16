f605ac0041d61b1193f652554c458d64752bdc9b
flywind <xzsflywind@gmail.com>
2020-08-16 08:57:25 +0800

    clean

--- tests/timer/t_timer.nim
+++ tests/timer/t_timer.nim
@@ -50,6 +50,7 @@ block:
   t.cancel(n1)
   # cancel multi times
   t.cancel(n1)
+  t.cancel(n1)
   t.poll(16)
   doAssert count == 0
 
@@ -69,57 +70,23 @@ block:
   doAssert count == 0, $count
 
 
-# block:
-#   var t = initTimer(100)
-#   var count = 0
-#   var event0 = initTimerEvent(proc() = 
-#     inc count)
+
+block:
+  var t = initTimer(10)
+  var count = 0
+  var event0 = initTimerEvent(proc() = 
+    inc count)
+
+  var a = t.add(event0, 20)
+  var b = t.add(event0, 15)
 
 
-#   var event1 = initTimerEvent(proc() = echo "first")
-#   var event2 = initTimerEvent(proc() = echo "second")
-
-#   discard t.add(event1, 10)
-#   discard t.add(event2, 1)
-
-#   poll(t, 100)
-#   discard t.add(event0, 5)
-
-#   discard t.add(event1, 9)
-#   echo t
-#   poll(t, 1000)
-#   doAssert count == 1, $count
-#   discard t.add(event0, 2)
-
-#   poll(t, 200)
-#   doAssert count == 2, $count
-
-# block:
-#   var t = initTimer(10)
-#   var count = 0
-#   var event0 = initTimerEvent(proc() = 
-#     inc count)
-
-
-#   var event1 = initTimerEvent(proc() = echo "first")
-#   var event2 = initTimerEvent(proc() = echo "second")
-
-#   var a = t.add(event1, 20)
-#   var b = t.add(event2, 15)
-
-#   echo t.wheel.slotsToString(1)
-#   echo t
-#   # t.cancel(b)
-#   poll(t, 100)
-#   poll(t, 30)
-#   echo t
-#   echo t.wheel.slotsToString(1)
-#   t.cancel(a)
-#   echo t
-#   echo t.wheel.slotsToString(0)
-#   poll(t, 200)
-#   echo t
-#   echo t.wheel.slotsToString(1)
+  t.cancel(b)
+  poll(t, 100)
+  poll(t, 30)
+  t.cancel(a)
+  poll(t, 200)
+  doAssert count == 0, $count
 
 # block:
 #   var t = initTimer(1)
